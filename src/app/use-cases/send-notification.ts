43ed93c0f6030d266c6eade41287b1e65206ed2d
Felipe Melo <felipefrmelo@hotmail.com>
2022-12-13 22:41:13 -0300

    create send notification use case, notifications repository

---
+++ src/app/use-cases/send-notification.ts
@@ -0,0 +1,29 @@
+import { Content } from "../entities/content";
+import { Notification } from "../entities/notification";
+import { NotificationsRepository } from "../repositories/notifications-repository";
+
+interface SendNotificationRequest {
+  recipientId: string;
+  content: string;
+  category: string;
+}
+
+interface SendNotificationResponse {
+  notification: Notification;
+}
+
+export class SendNotification {
+  constructor(private notificationsRepository: NotificationsRepository) { }
+
+  async execute(request: SendNotificationRequest): Promise<SendNotificationResponse> {
+    const notification = new Notification({
+      recipientId: request.recipientId,
+      content: new Content(request.content),
+      category: request.category,
+    });
+
+    await this.notificationsRepository.create(notification);
+
+    return { notification };
+  }
+}
